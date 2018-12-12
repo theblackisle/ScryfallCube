89dc1562e36a331d950a7c3b00f8a45cf5d07948
McBlackisle <theblackisle@naver.com>
2018-12-12 16:15:55 +0900

    add function commit

--- Main.py
+++ Main.py
@@ -2,33 +2,55 @@ import urllib.request
 import pprint
 import json
 
-client_id = "MY_CLIENT_ID"  # 애플리케이션 등록시 발급 받은 값 입력
-client_secret = "MY_CLIENT_SECRET"  # 애플리케이션 등록시 발급 받은 값 입력
+def scryprint(string, indent=0):
+    if string == list:
+        for index in string:
+            print(string)
+    elif string == dict:
+        pass
+    else:
+        print(string)
 
-#searchquery = input("search for: ")
-url = "https://api.scryfall.com/cards/search?&q=" + "is%3Afirstprint+oracle%3A" + "spelltwine" #searchquery
-#requested = urllib.request.Request(url)
-#response = urllib.request.urlopen(requested) #url can be either a string or a Request object.
-response = urllib.request.urlopen(url)
+def getCard(searchquery):
+    url = "https://api.scryfall.com/cards/search?&q=" + "is%3Afirstprint+oracle%3A" + searchquery
 
+    response = urllib.request.urlopen(url)
+    # requested = urllib.request.Request(url)
+    # response = urllib.request.urlopen(requested) 로 쓸수도 있음
+    # url can be either a string or a Request object.
 
-rescode = response.getcode()
-if (rescode == 200): #HTTP Status codes 200 == OK
-    response_body = response.read().decode('utf-8') # response_body is <class 'str'>
-    #type of reponse.read() == bytes
-    #type of response.read().decode() == str
+    # 참고
+    # type of reponse == http.client.HTTPResponse
+    # type of reponse.read() == bytes
+    # type of response.read().decode('utf-8') == str
+    # type of json.loads(s=response.read().decode('utf-8')) == dict
 
-    #json_dump = json.dumps(response_body) # json_dump is <class 'str'>
-    json_structure = json.loads(s=response_body) # json_structure is <class 'dict'> #인자로 response_body를 넣으면 pretty printing 됨. json_dump를 넣으면 안됨.
+    if (response.getcode() == 200):  # HTTP Status codes 200 == OK
+        json_structure = json.loads(s=response.read().decode('utf-8'))
+        # json_structure is <class 'dict'>
+        # 둘다 str이지만 인자로 response.read()를 넣어야지 json_dump를 넣으면 pretty printing 안됨.
+        if (json_structure["total_cards"]==1):  # 정확한 카드 매칭
+            return json_structure["data"][0]
+        elif (json_structure["total_cards"]>1):
+            print("/"%s/" has not unique search result: %d many cards are found" % (searchquery, json_structure["total_cards"]))
+        else:  # (json_structure["total_cards"]==0)
+            print("no such card: %s" % searchquery)
+    else:
+        print("Error Code:" + rescode)
 
+def prettyprint(data, indent=2, mode="pprint"):
+    if (mode=="pprint"):
+        pp = pprint.PrettyPrinter(indent)  # List object의 pretty printer.
+        pp.pprint(data)
+    elif (mode=="json.dump"):
+        print(json.dumps(data, indent=indent))
+    else:
+        print("no such print mode")
 
-    pp = pprint.PrettyPrinter(indent=4) #List object의 pretty printer.
-    pp.pprint(response.getheaders())
-    print("\n")
-    print("response_body is %s" % type(response_body))
-    #print("json_body is %s" % type(json_dump))
-    print("json_structure is %s" % type(json_structure))
-    print("\n")
-    print(json.dumps((json_structure), indent=4))
-else:
-    print("Error Code:" + rescode)
+# client_id = "MY_CLIENT_ID"  # 애플리케이션 등록시 발급 받은 값 입력
+# client_secret = "MY_CLIENT_SECRET"  # 애플리케이션 등록시 발급 받은 값 입력
+
+searchquery = input("search for: ")
+result = getCard(searchquery)
+
+prettyprint(result, 10, "json.dump")
